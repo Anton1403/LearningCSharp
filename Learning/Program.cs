6ca03acb4d97ef05bce7ba215b7bef88b8f7be94
Anton Bialenik <anton.bialenic@specific-group.com>
2019-01-10 12:05:45 +0300

    Created 2 project; the last time was working with loops and arrays

---
+++ Learning/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("A = ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("B = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (a > b && a % b == 0)
+                {
+                    Console.WriteLine($"{a} bigger than {b} and remainder equals 0");
+                }
+                else if (a > b && a % b != 0)
+                {
+                    Console.WriteLine($"{a} bigger than {b} and remainder don't equals 0");
+                }
+                else if (a < b && b % a == 0)
+                {
+                    Console.WriteLine($"{a} smaller than {b} and remainder equals 0");
+                }
+                else if (a < b && b % a != 0)
+                {
+                    Console.WriteLine($"{a} smaller than {b} and remainder don't equals 0");
+                }
+                else if ( a == b )
+                {
+                    Console.WriteLine($"{a} equals {b}");
+                }
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "Y":
+                    Console.WriteLine("Y");
+                    break;
+                case "N":
+                    Console.WriteLine("N");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+
+            int n = numbers.Length;
+            int x = n / 2;
+            int temp;
+            for(int i = 0; i < x; i++)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[n - i-1];
+                numbers[n - i-1] = temp;
+            }
+            foreach(int i in numbers)
+            {
+                Console.Write($"{i} \t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"Квадрат числа {i} равен {i * i}");
+            }
+
+
+
+            int a = 6;
+            while(a > 0)
+            {
+                Console.WriteLine(a);
+                a--;
+            }
+
+
+
+            for (int x = 0; x < 9; x++)
+            {
+                if (x == 5)
+                    continue;
+                Console.WriteLine(x);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
